d1a4ede5dcb88b98939d608cfbae6f29b853a6a4
DaviRain-Su <davirain.yin@gmail.com>
2020-12-12 13:56:55 +0800

    分组导入

--- src/a-tour-of-go/hello.go
+++ src/a-tour-of-go/hello.go
@@ -1,11 +1,20 @@
 package main
 
+
+
+//import (
+//	"fmt"
+//	"math/rand"
+//)
+
+// 分组导入
 import (
 	"fmt"
-	"math/rand"
+	"math"
 )
 
 func main()  {
-	fmt.Println("Hello, Wold!")
-	fmt.Println("My favorite number is ", rand.Intn(10))
+	//fmt.Println("Hello, Wold!")
+	//fmt.Println("My favorite number is ", rand.Intn(10))
+	fmt.Printf("Now you have %g problems.\n\n", math.Sqrt(9))
 }
