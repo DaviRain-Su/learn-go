c24d70ba42c55506545b1c401179e39788d1839c
Davirain <davirain.yin@gmail.com>
2022-12-10 16:18:04 +0800

    Add Guess

--- head-first-go/Chapter2/Guess.go
+++ head-first-go/Chapter2/Guess.go
@@ -1,1 +1,54 @@
 package Chapter2
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Guess challenges players to guess a random number.
+func Guess() {
+	second := time.Now().Unix()
+	rand.Seed(second)
+	target := rand.Intn(100) + 1
+	fmt.Println("I've chosen a random number between 1 and 100.")
+	fmt.Println("Can you guess it?")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	success := false
+	for guess := 0; guess < 10; guess += 1 {
+		fmt.Println("You have", 10-guess, "guesses left.")
+
+		fmt.Print("Male a guess: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH.")
+		} else {
+			success = true
+			fmt.Println("Good job! You guessed it!")
+			break
+		}
+	}
+
+	if !success {
+		fmt.Println("Sorry, you didn't guess my number. It was: ", target)
+	}
+}
--- head-first-go/main.go
+++ head-first-go/main.go
@@ -1,10 +1,9 @@
 package main
 
-import (
-	chapter2 "headfirstgo/Chapter2"
-)
+import chapter2 "headfirstgo/Chapter2"
 
 func main() {
-	chapter2.PassFail()
-	chapter2.FileSize()
+	//chapter2.PassFail()
+	//chapter2.FileSize()
+	chapter2.Guess()
 }
